6b5818630894e8d7c52f7099467184d2b4461aa4
slince <taosikai@yeah.net>
2017-04-18 23:39:50 +0800

    improve system v

---
+++ SystemV/IpcKeyTrait.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taosikai
+ * Date: 2017/4/18
+ * Time: 22:52
+ */
+
+namespace Slince\Process\SystemV;
+
+
+class IpcKeyTrait
+{
+
+}
---
+++ SystemV/MessageQueue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taosikai
+ * Date: 2017/4/18
+ * Time: 23:30
+ */
+
+namespace Slince\Process\SystemV;
+
+
+class MessageQueue
+{
+
+}
